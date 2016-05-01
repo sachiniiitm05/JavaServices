ce28ae8c369873e50598e6cd0cdd2127112430d8
Sachin <sachin_iiitm05@yahoo.com>
2016-05-01 12:48:01 -0500

    Added propeties file

--- src/main/java/com/lessonslab/cxfrestservice/dao/EmployeeDao.java
+++ src/main/java/com/lessonslab/cxfrestservice/dao/EmployeeDao.java
@@ -1,15 +1,23 @@
 package com.lessonslab.cxfrestservice.dao;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import com.lessonslab.cxfrestservice.model.Employee;
 
 @Component
-public class EmployeeDao 
-{
+//@Configuration
+public class EmployeeDao {
 	
-	public Employee getEmployeeDetails(String employeeId)
-	{
+	@Autowired
+	private Properties appProperties;
+
+	public Employee getEmployeeDetails(String employeeId) {
+		
+		System.out.println("Name from property : " + appProperties.getProperty("name"));
 		Employee emp = new Employee();
 		emp.setDateOfJoining("01-02-2001");
 		emp.setDepartment("Sales");
